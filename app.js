d57efe810bf6f0bcc31f04363ef7867926427949
Anthony Langford <anthony.langford@gmail.com>
2017-04-02 17:05:27 -0400

    added body-parser and emailjs

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const express = require('express');
-const morgan  = require('morgan');
-const app     = express();
+const express       = require('express');
+const morgan        = require('morgan');
+const bodyParser    = require("body-parser");
+const emailjs       = require('emailjs');
+const app           = express();
+
+const email = emailjs.server.connect({
+  user: "free.tony.bologna@gmail.com",
+  password:" luck65pwns",
+  host: "smtp.gmail.com",
+  ssl: true
+});
 
 let PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/public', express.static(__dirname + '/public'));
 
@@ -13,12 +24,24 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-  // if (!req.body) {
-  //   res.status(400).json({ error: 'invalid request: no data in POST body' });
-  //   return;
-  // }
+  if (!req.body) {
+    res.status(400).json({ error: 'invalid request: no data in POST body' });
+    return;
+  }
 
-  console.log ('req.body', req.body);
+  email.send('gmail', {
+    from: `${req.body.name} <${req.body.email}>`,
+    to: 'anthony.langford@gmail.com',
+    subject: "IMPORTANT ART THINGS!",
+    text: `${req.body.message}`
+  }, function(err, response) {
+    if (err) {
+     console.log('error', err);
+    } else {
+     console.log("email sent");
+    }
+  });
+
 });
 
 app.listen(PORT, () => {
