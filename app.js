edf15139785c77bee663a6d48bbad4571349d7e3
Anthony Langford <anthony.langford@gmail.com>
2017-04-02 17:46:56 -0400

    nodemailer

--- app.js
+++ app.js
@@ -21,14 +21,6 @@ let transporter = nodemailer.createTransport({
   }
 });
 
-// send mail with defined transport object
-transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-      return console.log(error);
-  }
-  console.log('Message %s sent: %s', info.messageId, info.response);
-});
-
 let PORT = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
